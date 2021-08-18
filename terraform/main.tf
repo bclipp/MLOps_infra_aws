ac78021172f72da3e7033ec81333b9dc8b8c2a6a
Brian Lipp <bclipp770@gmail.com>
2021-08-18 14:36:16 -0400

    adding zip

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# docker and zip
-
-
-# docker repo
-
-
-
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_ex_role"
   role = aws_iam_role.lambda_ex_role.id
@@ -19,9 +12,27 @@ resource "aws_iam_role" "lambda_ex_role" {
   assume_role_policy =  file("LambdaAssumeRole.json")
 }
 
+
+
+# docker and zip
+
+#resource "aws_lambda_function" "MLOps" {
+#  function_name = "MLOps"
+#  handler = "lambda_function.handler"
+#  role = "${aws_iam_role.lambda_ex_role.arn}"
+#  runtime = "python3.8"
+#  image_uri = ""
+#}
+
+# docker repo
+
+
+
+
 resource "aws_lambda_function" "MLOps" {
   function_name = "MLOps"
   handler = "lambda_function.handler"
   role = "${aws_iam_role.lambda_ex_role.arn}"
   runtime = "python3.8"
+  filename = "init_zip.zip"
 }
